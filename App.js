1e855ab08c575e4868e055ee6eddb12a439c2d16
minhthao <minhthao5648@gmail.com>
2021-03-05 23:47:53 +0700

    add ios

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect} from 'react';
-import {SafeAreaView, Text, StatusBar, Alert} from 'react-native';
+import {SafeAreaView, Text, StatusBar, Button, Platform} from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import axios from 'axios';
 import PushNotification from 'react-native-push-notification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 const App = () => {
   messaging().setBackgroundMessageHandler(async (remoteMessage) => {
@@ -14,7 +16,7 @@ const App = () => {
     if (fcmToken) {
       console.log(fcmToken);
       axios
-        .post('http://192.168.1.9:3000/', {token: fcmToken})
+        .post('http://192.168.1.22:3000/', {token: fcmToken})
         .then((res) => {
           console.log('res', res.data);
         })
@@ -38,24 +40,103 @@ const App = () => {
     }
   };
 
+  /// show notification via firebase
   useEffect(() => {
     requestUserPermission();
+
+    PushNotification.configure({
+      // (optional) Called when Token is generated (iOS and Android)
+      onRegister: function (token) {
+        console.log('TOKEN:', token);
+      },
+
+      // (required) Called when a remote is received or opened, or local notification is opened
+      onNotification: function (notification) {
+        console.log('NOTIFICATION:', notification);
+
+        // (required) Called when a remote is received or opened, or local notification is opened
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+
+      // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+      onAction: function (notification) {
+        console.log('ACTION:', notification.action);
+        console.log('NOTIFICATION:', notification);
+
+        // process the action
+      },
+
+      // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+      onRegistrationError: function (err) {
+        console.error(err.message, err);
+      },
+
+      // IOS ONLY (optional): default: all - Permissions to register.
+      permissions: {
+        alert: true,
+        badge: true,
+        sound: true,
+      },
+
+      // Should the initial notification be popped automatically
+      // default: true
+      popInitialNotification: true,
+
+      /**
+       * (optional) default: true
+       * - Specified if permissions (ios) and token (android and ios) will requested or not,
+       * - if not, you must call PushNotificationsHandler.requestPermissions() later
+       * - if you are not using remote notification or do not have Firebase installed, use this:
+       *     requestPermissions: Platform.OS === 'ios'
+       */
+      requestPermissions: true,
+    });
+
     const unsubscribe = messaging().onMessage(async (remoteMessage) => {
       // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
       console.log(remoteMessage);
+
+      if (Platform.OS === 'ios') {
+        PushNotificationIOS.presentLocalNotification({
+          alertBody: 'Test Notifi',
+          alertTitle: 'Test',
+        });
+        PushNotificationIOS.setApplicationIconBadgeNumber();
+      } else {
+        PushNotification.localNotification({
+          title: 'My Notification Title', // (optional)
+          message: 'My Notification Message', // (required)
+        });
+        // Show notificaiton number
+        PushNotification.setApplicationIconBadgeNumber();
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  // hard show notification
+  const handleNotification = () => {
+    if (Platform.OS === 'ios') {
+      PushNotificationIOS.presentLocalNotification({
+        alertBody: 'Test Notifi',
+        alertTitle: 'Test',
+      });
+    } else {
       PushNotification.localNotification({
         title: 'My Notification Title', // (optional)
         message: 'My Notification Message', // (required)
       });
-    });
-    return unsubscribe;
-  }, []);
+    }
+  };
 
   return (
     <>
       <StatusBar />
       <SafeAreaView>
-        <Text>Demo Notification</Text>
+        <Button
+          title={'Shown notificaion'}
+          onPress={() => handleNotification()}
+        />
       </SafeAreaView>
     </>
   );
--- ios/DemoNotification/AppDelegate.m
+++ ios/DemoNotification/AppDelegate.m
@@ -3,6 +3,8 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <UserNotifications/UserNotifications.h>
+#import <RNCPushNotificationIOS.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -54,5 +56,30 @@ static void InitializeFlipper(UIApplication *application) {
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+//custom push notification for ios
+// Required for the register event.
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+ [RNCPushNotificationIOS didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+// Required for the notification event. You must call the completion handler after handling the remote notification.
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+  [RNCPushNotificationIOS didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+// Required for the registrationError event.
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+ [RNCPushNotificationIOS didFailToRegisterForRemoteNotificationsWithError:error];
+}
+// Required for localNotification event
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)(void))completionHandler
+{
+  [RNCPushNotificationIOS didReceiveNotificationResponse:response];
+}
+
 
 @end
